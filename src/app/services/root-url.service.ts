dc277e82b07e2a598f39d9d51598c3fa8416ce94
nikistefanov <nikolay.n.stefanov@gmail.com>
2019-09-24 10:55:34 +0300

    responsive styling; quote font; using B4 utilities

--- src/app/services/root-url.service.ts
+++ src/app/services/root-url.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from "@angular/core";
 @Injectable()
 export class RootUrlService {
     public getUrl() {
-        return "http://10.211.55.3:3667";
+        return "http://localhost:9090";
     }
 }
